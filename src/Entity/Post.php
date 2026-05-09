86e74c615ae4d0eba2424ade6b03a89c5497975d
Nico Zeier <nico.zeier@znit.org>
2026-05-09 03:00:51 +0200

    Fix attribute name: tableName -> table

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -11,7 +11,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: PostRepository::class, tableName: 'bytes_commerce_blog_post')]
+#[ORM\Entity(repositoryClass: PostRepository::class, table: 'bytes_commerce_blog_post')]
 class Post implements TimeAwareInterface
 {
     use TimeAwareTrait;
