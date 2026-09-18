80de8722e2528a697f071923da7933a301f06ae8
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2026-09-19 00:05:48 +0200

    BC-XX Adapt the service.yaml to make the services publicly available.

--- src/DependencyInjection/EasyBlogExtension.php
+++ src/DependencyInjection/EasyBlogExtension.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace BytesCommerce\EasyBlog\DependencyInjection;
 
-use BytesCommerce\EasyBlog\Repository\CategoryRepository;
-use BytesCommerce\EasyBlog\Repository\CategoryRepositoryInterface;
-use BytesCommerce\EasyBlog\Repository\FaqRepository;
-use BytesCommerce\EasyBlog\Repository\FaqRepositoryInterface;
-use BytesCommerce\EasyBlog\Repository\PostRepository;
-use BytesCommerce\EasyBlog\Repository\PostRepositoryInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -28,34 +22,10 @@ final class EasyBlogExtension extends Extension
         );
         $loader->load('services.yaml');
 
-        // Store configuration as parameters (guard against missing optional nodes)
         $container->setParameter('easy_blog.user.entity', $config['user_entity'] ?? 'App\\Entity\\User');
         $container->setParameter('easy_blog.vich_uploader', $config['vich_uploader'] ?? []);
         $container->setParameter('easy_blog.cache', $config['cache'] ?? []);
         $container->setParameter('easy_blog.pagination', $config['pagination'] ?? []);
-
-        // Register repository interfaces with implementations
-        $this->registerRepositories($container);
-
-        // Configure Doctrine entity mapping from XML files
-        $this->configureXmlMapping($container);
-    }
-
-    private function registerRepositories(ContainerBuilder $container): void
-    {
-        $container->register(CategoryRepositoryInterface::class, CategoryRepository::class)
-            ->setPublic(true);
-
-        $container->register(FaqRepositoryInterface::class, FaqRepository::class)
-            ->setPublic(true);
-
-        $container->register(PostRepositoryInterface::class, PostRepository::class)
-            ->setPublic(true);
-    }
-
-    private function configureXmlMapping(ContainerBuilder $container): void
-    {
-        // Mapping is handled via Doctrine configuration (attributes) in this project
     }
 
     public function getAlias(): string
